2d31ecb12697b78ed9e717b6cf6dab3c823f5348
kellyabud <135430840+kellyabud@users.noreply.github.com>
2024-02-02 01:25:14 -0300

    Create e2e tests with cypress

---
+++ cypress/support/e2e.js
@@ -0,0 +1,9 @@
+
+// Import commands.js using ES2015 syntax:
+import 'cypress-iframe'
+import "cypress-mailosaur"
+
+import './commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
